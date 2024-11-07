f4252a82621ba760c4342421bb552f04fdc44c37
DawidJanczak <dawid.janczak@gmail.com>
2024-11-07 08:42:10 +0800

    Updates

--- code.js
+++ code.js
@@ -1,9 +1,11 @@
+const UPGRADES_URL = "https://api.jsonbin.io/v3/qs/672b3c19acd3cb34a8a39af3"
+
 document.addEventListener("DOMContentLoaded", () => {
-  window.Stimulus.register("hello", class extends Controller {
-    static targets = ["name"]
+  window.Stimulus.register("upgrades", class extends Controller {
+    static targets = ["hello"]
 
     connect() {
-      this.nameTarget.value = "hiya!"
+      this.helloTarget.textContent = "Hello! 😃"
     }
   })
 })
