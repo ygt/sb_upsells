05732f0ecf2d7151167b405e9320854e4f25437a
DawidJanczak <dawid.janczak@gmail.com>
2024-11-08 20:23:52 +0800

    Update the url

--- code.js
+++ code.js
@@ -1,10 +1,10 @@
 const { createRoot } = ReactDOM
 
-const UPGRADES_URL = "https://api.jsonbin.io/v3/qs/672ca552e41b4d34e45018b4"
+const UPGRADES_URL = "https://api.jsonbin.io/v3/qs/672dffa8e41b4d34e450cad7"
 
 const Hello = (props) => {
   return <div>Hello {props.name}! 😃</div>
 }
 
 const root = createRoot(document.getElementById("root"))
-root.render(<Hello name="World"/>)
+root.render(<Hello name="World" />)
