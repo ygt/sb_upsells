60b0410bcda3674a6fb5595f851a3f172f39df7d
DawidJanczak <dawid.janczak@gmail.com>
2024-11-08 20:13:36 +0800

    Update the url

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-const UPGRADES_URL = "https://api.jsonbin.io/v3/qs/672ca552e41b4d34e45018b4"
+const UPGRADES_URL = "https://api.jsonbin.io/v3/qs/672dffa8e41b4d34e450cad7"
 
 document.addEventListener("DOMContentLoaded", () => {
   window.Stimulus.register("upgrades", class extends Controller {
