a259135d3dc14925276b836bb2ec143b7410697d
Mike Charlton <mike.charlton@yourgolftravel.com>
2024-11-13 20:57:09 +0900

    Update api url

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-const UPGRADES_URL = "https://api.jsonbin.io/v3/qs/6734938facd3cb34a8a7e8a5"
+const UPGRADES_URL = "https://api.jsonbin.io/v3/qs/6734937bad19ca34f8c96b8d"
 
 document.addEventListener("DOMContentLoaded", () => {
   window.Stimulus.register("upgrades", class extends Controller {
